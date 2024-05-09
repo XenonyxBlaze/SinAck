d6a50871baf768d500cc7b55258ab9b6400a2e15
Aarav <aarav.rajput2003@gmail.com>
2024-05-09 18:20:23 +0530

    Scanning functionality

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 
+import os
+
 import goodsniffer
+
+import multiprocessing
 
 app = Flask("SinAck") 
 CORS(app)
+
+global scan_status
+global scan_link
+
+scan_status = "Start Scan"
+scan_link = "/scan"
 
 app.config.update( 
     SECRET_KEY='dev'
 )
 
 @app.route('/')
-def index_function(): 
-    return render_template('index.html')
+def index_function():
+    return render_template('index.html', scanbtn=scan_status, scanlink=scan_link)
+    
 
 @app.route('/scan')
 def scan_function():
-    goodsniffer.main
+    global snifferProcess
+    snifferProcess = multiprocessing.Process(target=goodsniffer.main)
+    snifferProcess.start()
+    global scan_status
+    global scan_link
+    scan_status = "Stop Scan"
+    scan_link = "/stopscan"
+    return app.redirect('/')
+
+@app.route('/stopscan')
+def stop_scan():
+    snifferProcess.terminate()
+    global scan_status
+    global scan_link
+    scan_status = "Start Scan"
+    scan_link = "/scan"
+    return app.redirect('/')
+
+# @app.route('/stopscan')
+# def stop_scan_function():
+
 
 
 if __name__ == '__main__':
